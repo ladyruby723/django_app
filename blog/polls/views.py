9ce69d29d441fcaff6009f8c0e1e2780219793da
Kate Richardson <krichardson723@gmail.com>
2017-03-27 19:49:04 -0600

    add views and detail/results/vote URL patterns in polls

--- blog/polls/views.py
+++ blog/polls/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+# Create views
 
 def index(request):
     return HttpResponse("This is a blog/polls application, built with the Django web framework for Python.")
 
+def detail(request, question_id):
+    return HttpResponse("Question %s." % question_id)
+
+def results(request, question_id):
+    response = "Results for question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Voting on question %s." % question_id)
